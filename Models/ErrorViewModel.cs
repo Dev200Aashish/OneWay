12ac0b4aa48a6cab74f4e45d1682ec56a097663b
Dev200Aashish <devashu0016@gmail.com>
2023-12-24 21:10:59 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitHost.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
